ba43c176148b371cb36bcfdb4f1904e037171a07
danielvazsonyi <daniel.vazsonyi@balabit.com>
2021-04-04 19:49:03 +0200

    feat(recipe-card): add vue.use helper and displaying some dummy data

--- src/components/main-content/recipe-card/RecipeCard.vue
+++ src/components/main-content/recipe-card/RecipeCard.vue
@@ -1,10 +1,28 @@
 <template>
-    <div>
-      {{name}}
+    <div class="container">
+      <md-card>
+        <md-card-header>
+          <div class="md-title">{{name}}</div>
+        </md-card-header>
+
+        <md-card-content>
+          Some fancy thing should come here.
+        </md-card-content>
+
+        <md-card-actions>
+          <md-button>a</md-button>
+          <md-button>b</md-button>
+        </md-card-actions>
+      </md-card>
     </div>
 </template>
 
 <script>
+import {MdCard, MdButton} from 'vue-material/dist/components';
+import {usePlugins} from '@/components/helpers/vue-use.helper';
+
+usePlugins([MdCard, MdButton]);
+
 export default {
   name: 'recipe-card',
   props: {
@@ -17,4 +35,7 @@ export default {
 
 <style scoped>
 
+.container {
+  margin: 8px;
+}
 </style>
