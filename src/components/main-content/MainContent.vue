124579179f17e4b8f1900a5ef9cb1d079adfe393
danielvazsonyi <daniel.vazsonyi@balabit.com>
2021-04-08 19:33:48 +0200

    feat(recipe-card): add flex display and displaying recipes basic data

--- src/components/main-content/MainContent.vue
+++ src/components/main-content/MainContent.vue
@@ -1,11 +1,17 @@
 <template>
   <div>
-    <recipe-card :info="recipeInfo"/>
+    <section v-if="recipes">
+      <recipe-card v-for="recipe of recipes" :info="recipe" :key="recipe.label"/>
+    </section>
+    <button @click="fetchRecipes">Fetch</button>
   </div>
 </template>
 
 <script>
 import RecipeCard from '@/components/main-content/recipe-card/RecipeCard';
+import axios from 'axios';
+
+const apiUrl = 'https://api.edamam.com/search';
 
 export default {
   name: 'main-content',
@@ -14,15 +20,28 @@ export default {
   },
   data() {
     return {
-      recipeInfo: {
-        name: 'Burger',
-        description: 'A fancy burger with cheese'
-      }
+      recipes: null
+    };
+  },
+  methods: {
+    fetchRecipes: function () {
+      const params = {
+        q: 'chicken'
+      };
+      axios.get(apiUrl, {params}).then(response => {
+        console.log(response.data.hits);
+        this.recipes = response?.data?.hits;
+      });
     }
   }
-}
+};
+
 </script>
 
 <style scoped>
-
+section {
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+}
 </style>
--- src/components/main-content/recipe-card/RecipeCard.vue
+++ src/components/main-content/recipe-card/RecipeCard.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="container">
-    <md-card>
+    <md-card class="card" v-bind:style="{ 'background-image': 'url(' + info.recipe.image + ')' }">
       <md-card-header>
-        <div class="md-title">{{ info.name }}</div>
+        <div class="md-title">{{ info.recipe.label }}</div>
       </md-card-header>
       <md-card-content>
-        {{ info.description }}
+
       </md-card-content>
     </md-card>
   </div>
@@ -17,12 +17,18 @@ import {usePlugins} from '@/helpers/vue-use.helper';
 
 usePlugins([MdCard, MdButton]);
 
+const image = "https://www.edamam.com/web-img/e42/e42f9119813e890af34c259785ae1cfb.jpg";
 export default {
   name: 'recipe-card',
   props: {
     info: {
       type: Object
     }
+  },
+  data () {
+    return {
+      image
+    };
   }
 };
 </script>
@@ -31,7 +37,14 @@ export default {
 
 .container {
   margin: 8px;
-  border-radius: 4px;
+  border-radius: 16px;
   box-shadow: 1px 3px #dad8d8;
+  height: 350px;
+}
+
+.card {
+  color: white;
+  background-size: cover;
+  height: 75%;
 }
 </style>
