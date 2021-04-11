d58a89c6fa98f4748624bced2880de82c0437173
danielvazsonyi <daniel.vazsonyi@balabit.com>
2021-04-11 10:15:43 +0200

    feat(recipe-card): display details for the recipe
    Display details of recipe and add style.

--- src/components/main-content/recipe-card/RecipeCard.vue
+++ src/components/main-content/recipe-card/RecipeCard.vue
@@ -1,35 +1,27 @@
 <template>
   <div class="container">
-    <md-card class="card" v-bind:style="{ 'background-image': 'url(' + info.recipe.image + ')' }">
-      <md-card-header>
-        <div class="md-title">{{ info.recipe.label }}</div>
-      </md-card-header>
-      <md-card-content>
-
-      </md-card-content>
-    </md-card>
+    <md-card class="card" v-bind:style="{ 'background-image': 'url(' + info.recipe.image + ')' }"></md-card>
+    <recipe-info :info="info"></recipe-info>
   </div>
 </template>
 
 <script>
-import {MdCard, MdButton} from 'vue-material/dist/components';
+import {MdCard} from 'vue-material/dist/components';
 import {usePlugins} from '@/helpers/vue-use.helper';
+import RecipeInfo from './RecipeInfo';
 
-usePlugins([MdCard, MdButton]);
+usePlugins([MdCard]);
 
-const image = "https://www.edamam.com/web-img/e42/e42f9119813e890af34c259785ae1cfb.jpg";
 export default {
   name: 'recipe-card',
+  components: {
+    RecipeInfo
+  },
   props: {
     info: {
       type: Object
     }
   },
-  data () {
-    return {
-      image
-    };
-  }
 };
 </script>
 
@@ -37,16 +29,17 @@ export default {
 
 .container {
   flex: 1 300px;
-  height: 40vh;
+  height: 38vh;
   min-height: 300px;
   margin: 8px;
   border-radius: 16px;
-  box-shadow: 1px 3px #dad8d8;
+  box-shadow: 0 1px #dad8d8;
 }
 
 .card {
-  color: white;
-  background-size: cover;
   height: 75%;
+  background-size: cover;
+  background-position: center;
+  border-radius: 24px;
 }
 </style>
---
+++ src/components/main-content/recipe-card/RecipeInfo.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <div class="name">
+      <strong>{{ info.recipe.label }}</strong>
+    </div>
+    <div class="info">
+      <span class="yield">
+        <md-icon :md-src="require('@/assets/face_black_24dp.svg')"></md-icon>
+        {{ info.recipe.yield }}
+      </span>
+      <span class="time">
+        <md-icon :md-src="require('@/assets/alarm_black_24dp.svg')"></md-icon>
+        {{ info.recipe.totalTime }}'
+      </span>
+      <span class="labels">
+        <md-chip v-for="label of info.recipe.dietLabels" :key="label">{{ label }}</md-chip>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+import {MdIcon, MdChips} from 'vue-material/dist/components';
+import {usePlugins} from '@/helpers/vue-use.helper';
+
+usePlugins([MdIcon, MdChips]);
+export default {
+  name: 'recipe-info',
+  props: {
+    info: {
+      type: Object
+    }
+  }
+}
+</script>
+
+<style scoped>
+.info {
+  display: grid;
+  grid-template-rows: 50px;
+  grid-template-columns: 50px 50px 1fr;
+  column-gap: 6px;
+  align-items: center;
+  grid-template-areas: "yield time labels";
+}
+
+.name {
+  margin: 8px 0 0 8px;
+}
+
+.md-svg-loader {
+  display: inline-block;
+  height: 18px;
+  line-height: unset;
+}
+
+.yield {
+  grid-area: yield;
+  justify-self: center;
+  font-size: 12px;
+}
+
+.time {
+  grid-area: time;
+  justify-self: center;
+  font-size: 12px;
+}
+
+
+.labels {
+  grid-area: labels;
+}
+</style>
