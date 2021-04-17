279414f9c8d04e16d4f181f404990a9fbc5ab6c8
vazsonyidl <danielvazsonyi@gmail.com>
2021-04-17 15:05:18 +0200

    feat(recipe): add search icon, style and search button for the component

--- src/components/search/Search.vue
+++ src/components/search/Search.vue
@@ -1,8 +1,11 @@
+<!--suppress CssUnknownTarget -->
 <template>
-  <form>
+  <form class="form-container">
     <md-field>
       <label>Search expression(s)</label>
-      <md-input v-model="searchExpression" placeholder="Enter your search keyword(s)"></md-input>
+      <img src="../../assets/search_black_24dp.svg" alt="search-icon"/>
+      <md-input v-model="searchExpression" placeholder="Type in what you are looking for"></md-input>
+      <button type="button" class="search-button" @click="search()">Search</button>
     </md-field>
   </form>
 </template>
@@ -16,7 +19,13 @@ export default {
   name: 'search',
   data: () => ({
     searchExpression: ''
-  })
+  }),
+  methods: {
+    search: function() {
+      this.$store.commit('recipe/changeSearchExpression', this.searchExpression);
+      console.log(this.$store.state.recipe.searchExpression);
+    }
+  }
 };
 </script>
 
